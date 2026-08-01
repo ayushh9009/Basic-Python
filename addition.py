b9f9cc6732db49b47ec8b00e519911a13f975184
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-01 23:55:15 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,6 +1,5 @@
 # taking input from the user
 a = int(input("Enter the first number:"));
 b = int(input("Enter the second number:"));
-#  logic for addition of numbers
-sum = a+b;
-print(sum);
+#  logic for addition of numbers and output
+print("Sum is:", a + b)
