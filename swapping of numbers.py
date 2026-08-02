9a061647458a7ddc2e8db119296eb89cfc17e7f2
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-02 19:41:18 +0530

    Add files via upload

---
+++ swapping of numbers.py
@@ -0,0 +1,9 @@
+# taking input from the user
+a = input("Enter the value of a:")
+b = input("Enter the value of b:")
+# storing value of a in a variable
+temp = a
+a = b
+b = temp
+print("a=" + a)
+print("b=" + b)
