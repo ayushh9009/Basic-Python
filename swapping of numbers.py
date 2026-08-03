6d30445e16bb682e394c38814a957fff8fbfec20
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-03 18:59:25 +0530

    Update swapping of numbers.py

--- swapping of numbers.py
+++ swapping of numbers.py
@@ -1,8 +1,9 @@
 # taking input from the user
 a = input("Enter the value of a:")
 b = input("Enter the value of b:")
-# storing value of a in a variable
+# storing value of a in a temporary variable
 temp = a
+# swapping the values of 
 a = b
 b = temp
 print("a=" + a)
