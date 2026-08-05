5be90e39122a57e09c2dcfc0040f7576eb4a76e6
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-05 22:29:50 +0530

    Update swapping of numbers.py

--- swapping of numbers.py
+++ swapping of numbers.py
@@ -3,7 +3,7 @@ a = input("Enter the value of a:")
 b = input("Enter the value of b:")
 # storing value of a in a temporary variable
 temp = a
-# swapping the values of 
+# swapping the values of the variable 
 a = b
 b = temp
 print("a=" + a)
