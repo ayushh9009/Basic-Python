f612dcbbe4d0b2c05913b9a155ccec65ff1e99f8
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-10 22:14:13 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,4 +1,5 @@
 str = "this is a string"
-print(len(str))
-
+print(len(str))             # tells the length of the string
+print(str.endswith("ing"))  # tells whether it ends with 'ing' or not
+print(str.count("i")        # count total 'i' present in the string
 
