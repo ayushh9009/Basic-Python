3af64d8cefc2b51230ea06d6ccfdb78049e93e9c
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-13 22:12:44 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+marks = [23,34,34,35,39,55,67]
+print(marks) 
+print(type(marks))            # tells typw of data type       
+print(len(marks))             # tells length of the list 
+print(marks[3])
+marks[2]="45"                 # adding it to the list as list are mutable 
+print(marks)
+print(marks[1:3])             # list is mutable
+print(marks[1:])
