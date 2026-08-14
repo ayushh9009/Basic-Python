6829e61572a3217c555f87c670eec0f1cf10b8fd
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-14 22:43:28 +0530

    Update list.py

--- list.py
+++ list.py
@@ -7,3 +7,9 @@ marks[2]="45"                 # adding it to the list as list are mutable
 print(marks)
 print(marks[1:3])             # list is mutable
 print(marks[1:])
+
+
+list = [2,4,5,6,7]
+# print(list)
+list.append(10)                # adds 10 to the list
+print(list)
