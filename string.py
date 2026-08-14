c74ee8d304e65c3291aaf5e18e6a675f8f87075a
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-14 22:52:22 +0530

    Update string.py

--- string.py
+++ string.py
@@ -9,8 +9,9 @@ print(str1.count("i")        # count total 'i' present in the string
 
 // string slicing 
 str = "Amazing"
-print(str[0:3])              # prints from starting index 0 and ending index 3
-print(str[:4])               # prints from starting index 0 and ending index 4
-print(str[3:])               # prints from starting index 3 and till last index
-print(str[-3:-1])            # prints from starting index -4 and ending index -1
-print(str.replace("i","o"))  # replace the word "i" with "o" and prints the string
+print(str[0:3])                  # prints from starting index 0 and ending index 3
+print(str[:4])                   # prints from starting index 0 and ending index 4
+print(str[3:])                   # prints from starting index 3 and till last index
+print(str[-3:-1])                # prints from starting index -4 and ending index -1
+print(str.replace("i","o"))      # replace the word "i" with "o" and prints the string
+print(str.upper())               # capitalizes the string         
