90ee6e605e8b4f13f030a6ec2cf6c36288f21ffd
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-17 22:08:19 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,6 +1,6 @@
 marks = [23,34,34,35,39,55,67]
 print(marks)                  # prints the list
-print(type(marks))            # tells typw of data type       
+print(type(marks))            # tells type of data type       
 print(len(marks))             # tells length of the list 
 print(marks[3])
 marks[2]="45"                 # adding it to the list as list are mutable 
