c9f9ede59e8db880544156034945f360f6b27dd5
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-20 18:26:52 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -2,4 +2,4 @@
 
 a = (2,3,4,5,6)
 print(type(a))            # tells the datatype
-print(len(a))             # prints the total no of elements in 'a'
+print(len(a))             # prints the total number of elements in 'a'
