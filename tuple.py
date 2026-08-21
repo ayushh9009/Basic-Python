f102952ec95e1e699d1ece8763afe0efbd469096
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-21 22:39:53 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -3,3 +3,4 @@
 a = (2,3,4,5,3,6)
 print(type(a))            # tells the datatype
 print(len(a))             # prints the total number of elements in 'a'
+print(a.count(3))         # counts total times element 3 
