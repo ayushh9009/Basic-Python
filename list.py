a741ed5a7dc17b537f8e9e07178dd17bd40cb5ba
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-23 20:02:27 +0530

    Remove commented code and clean up list operations

--- list.py
+++ list.py
@@ -10,7 +10,7 @@ print(marks[1:])              # prints list from first index
 
 
 list = [2,4,5,6,7]
-# print(list)
+print(list)
 list.append(10)                # adds 10 to the list
 print(list)
 list.reverse()                 # reverse the list in reverse 
