d1088fda3000eed3e1411200a6a797c5b0c650bc
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-25 22:25:59 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -7,4 +7,4 @@ print(a.count(3))         # counts total times element 3
 print(a.index(4))         # tells the index of element 4
 print(max(a))             # prints largest number in the tuple
 print(sorted(a))          # prints sorted tuple
-print(a.index(3, 2))
+print(a.index(3, 2))      # prints the index of second index of element 3 
