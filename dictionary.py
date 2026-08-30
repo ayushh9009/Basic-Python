08fc90ef938510e9dd10633c58da46761ce6c17a
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-30 22:27:20 +0530

    Fix formatting and update dictionary methods

--- dictionary.py
+++ dictionary.py
@@ -11,4 +11,4 @@ student = {
 print(list(student.keys()))                         # prints keys of dictionary
 print(list(student.values()))                       # prints values  
 print(student.update({"city" : "delhi"})            # added city in student
-print(student.items())                             
+print(student.items())                              # prints key-value pair    
