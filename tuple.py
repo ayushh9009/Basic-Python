388f7cd2736c92694c269e2799acaf3f486d085c
ayushh9009 <ayushchaurasia717@gmail.com>
2026-08-31 22:09:45 +0530

    Add min function to tuple operations
    Added min function to find the smallest number in the tuple.

--- tuple.py
+++ tuple.py
@@ -6,6 +6,7 @@ print(len(a))             # prints the total number of elements in 'a'
 print(a.count(3))         # counts total times element 3 
 print(a.index(4))         # tells the index of element 4
 print(max(a))             # prints largest number in the tuple
+print(min(a))
 print(sorted(a))          # prints sorted tuple and do not change original tuple
 print(sort(a))            # prints sorted tuple and it changes the original tuple 
 print(a.index(3, 2))      # prints the index of second index of element 3 
