af8690a3fb1010f852460aa6946838404dd997b5
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-01 19:36:57 +0530

    Correct string operations and enhance examples
    Fixed syntax errors and updated string slicing examples.

--- string.py
+++ string.py
@@ -8,14 +8,15 @@ print(str1.count("i")        # count total 'i' present in the string
 
 
 # string slicing
-str = "Amazing"
-print(str[0:3])                  # prints from starting index 0 and ending index 3
-print(str[:4])                   # prints from starting index 0 and ending index 4
-print(str[3:])                   # prints from starting index 3 and till last index
-print(str[-3:-1])                # prints from starting index -4 and ending index -1
-print(str[::-1])                 # prints the reversal of string
-print(str.replace("i","o"))      # replace the word "i" with "o" and prints the string
-print(str.upper())               # capitalizes first word of the string   
-print(str.lower())               # Output: "amazing"
-print(str.startswith("Am"))      # Output is False
-print(str.find("z")              # returns index of element "z"                 
+str = "Amazing World"
+print(str[0:3])                           # prints from starting index 0 and ending index 3
+print(str[:4])                            # prints from starting index 0 and ending index 4
+print(str[3:])                            # prints from starting index 3 and till last index
+print(str[-3:-1])                         # prints from starting index -4 and ending index -1
+print(str[::-1])                          # prints the reversal of string
+print(str.replace("i","o"))               # replace the word "i" with "o" and prints the string
+print(str.upper())                        # capitalizes first word of the string   
+print(str.lower())                        # Output: "amazing"
+print(str.startswith("Am"))               # Output is False
+print(str.find("z")                       # returns index of element "z"                 
+print(str.replace("World", "Python")      # replace 'World' with 'Python'
