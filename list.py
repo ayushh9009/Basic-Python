a0274c5dd68c942ce1115b9bad01b542007582b1
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-05 22:04:13 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,8 +3,7 @@ print(marks)                  # prints the list
 print(type(marks))            # tells type of data type       
 print(len(marks))             # tells length of the list 
 print(marks[3])
-marks[2]="45"                 # adding it to the list as list are mutable 
-print(marks)
+print(marks[2])="45"          # adding it to the list as list are mutable 
 print(marks[1:3])             # list is mutable
 print(marks[1:])              # prints list from first index 
 
