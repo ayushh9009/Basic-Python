e686815dd08b1461fee349bf4991818449d332a1
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-05 22:03:26 +0530

    Fix syntax errors in dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -10,6 +10,6 @@ student = {
 }
 print(list(student.keys()))                         # prints keys of dictionary
 print(list(student.values()))                       # prints values  
-print(student.update({"city" : "delhi"})            # added city in student
+print(student.update({"city" : "delhi"}))           # added city in student
 print(student.items())                              # prints key-value pair    
 print(student.get("name"))                          # prints the name 
