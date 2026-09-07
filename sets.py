01d1ba9260e0233bc84c9d27041f8646cc08b563
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-07 19:28:25 +0530

    Format sets.py for consistency

--- sets.py
+++ sets.py
@@ -1,6 +1,6 @@
 collection = {1,2,3,'hello', 2,3}
 print(collection)
 print(type(collection))                         # prints type of datatype
-print(len(collection))                          
+print(len(collection))                          # prints the length                        
 print(collection.add(8))
 
