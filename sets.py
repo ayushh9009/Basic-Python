09a3e9655a792efd1c8f6d947fbe1d7c2d751285
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-07 19:27:05 +0530

    Update set operations in sets.py
    Refactor set operations and add a new element.

--- sets.py
+++ sets.py
@@ -1,4 +1,6 @@
 collection = {1,2,3,'hello', 2,3}
 print(collection)
 print(type(collection))                         # prints type of datatype
-print(len(collection))
+print(len(collection))                          
+print(collection.add(8))
+
