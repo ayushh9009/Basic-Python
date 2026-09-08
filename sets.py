75320ef4bc56aa36a665c791fc1038d21fae7f68
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-08 20:12:26 +0530

    Enhance set operations with removal functionality
    Added removal of element from the set and ensured proper formatting.

--- sets.py
+++ sets.py
@@ -3,4 +3,4 @@ print(collection)
 print(type(collection))                         # prints type of datatype
 print(len(collection))                          # prints the length                        
 print(collection.add(8))
-
+print(collection.remove(2))
