eb7e304180fdc8c65a260b793651f7e749ed386f
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-08 20:14:59 +0530

    Modify collection operations in sets.py
    Added a remove operation to the collection and printed the updated collection.

--- sets.py
+++ sets.py
@@ -4,3 +4,5 @@ print(type(collection))                         # prints type of datatype
 print(len(collection))                          # prints the length                        
 collection.add(8)
 print(collection)
+collection.remove(2)
+print(collection)
