ada535a0cbabcf8bdd4b55a1285e79c4c02e22fe
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-08 20:14:27 +0530

    Refactor collection operations in sets.py
    Removed print statements for collection operations and added them back with proper formatting.

--- sets.py
+++ sets.py
@@ -2,5 +2,5 @@ collection = {1,2,3,'hello', 2,3}
 print(collection)
 print(type(collection))                         # prints type of datatype
 print(len(collection))                          # prints the length                        
-print(collection.add(8))
-print(collection.remove(2))
+collection.add(8)
+print(collection)
