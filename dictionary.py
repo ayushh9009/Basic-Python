1ade39561f64f4fe5a6f7ebf8774dd2b259fd1e1
ayushh9009 <ayushchaurasia717@gmail.com>
2026-09-12 21:39:17 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -13,5 +13,5 @@ print(list(student.values()))                       # prints values
 print(student.update({"city" : "delhi"}))           # added city in student
 print(student.items())                              # prints key-value pair    
 print(student.get("name"))                          # prints the name 
-student["subjects"]["math"] = 75
+student["subjects"]["math"] = 75                    # updated the value
 print(student)
